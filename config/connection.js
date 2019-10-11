2fc7c95c1d2a83931ce2ac40d3d8bf80f3295331
gerritt <gmocore@gmail.com>
2019-10-10 18:13:38 -0600

    added jaws db connection

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 // set up connection to database
 const sql = require("mysql");
+let connection;
 
-// connetion and credentials to export
-const connection = sql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "gerritt",
-  database: "burger_db",
-  port: 3306
-});
+if (process.env.JAWSDB_URL) {
+  connection = sql.createConnection(process.env.JAWSDB_URL);
+} else {
+  // connection and credentials to export
+  connection = sql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "gerritt",
+    database: "burger_db",
+    port: 3306
+  });
+}
 
 // connection to db
 connection.connect(err => {
@@ -16,8 +21,8 @@ connection.connect(err => {
     console.error("error connecting: " + err.stack);
     return;
   }
-  console.log(`connected as id ${connection.threadId}`)
-})
+  console.log(`connected as id ${connection.threadId}`);
+});
 
 //export to use with orm
 module.exports = connection;
